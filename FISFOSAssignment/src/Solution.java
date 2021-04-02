861c9e88e84c7ef947130f76bec327a72e4bbd12
Lalit Solanki <55562345+imsolanki@users.noreply.github.com>
2021-04-03 05:14:11 +0530

    Update Solution.java

--- FISFOSAssignment/src/Solution.java
+++ FISFOSAssignment/src/Solution.java
@@ -17,12 +17,17 @@ public class Solution {
         fileName2 = sc.nextLine();
     }
 
-    void creatingFiles() throws FileNotFoundException {
+    void creatingFiles() throws IOException {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter data you wanted to store: ");
+        String s =sc.nextLine();
         FileOutputStream fo1 = new FileOutputStream(path+"\\"+fileName1,false);
-        FileOutputStream fo2 =new FileOutputStream(path+"\\"+fileName2,false);
+        byte[]array = s.getBytes();
+        fo1.write(array);
+        checkingFiles();
     }
     void checkingFiles() throws IOException {
-        File f =new File(path+" "+fileName1);
+        File f =new File(path+"\\"+fileName1);
         if(f.exists()){
             if(f.isFile())
             {
@@ -38,35 +43,15 @@ public class Solution {
         fis1.read(arr);
         String s =new String(arr,0,arr.length);
         System.out.println(s);
-
-
+        FileOutputStream fo2 =new FileOutputStream(path+"\\"+fileName2,false);
+        byte[]array = s.getBytes();
+        fo2.write(array);
     }
 
 
     public static void main(String[] args) throws IOException {
         Solution s = new Solution();
         s.creatingFiles();
-        s.checkingFiles();
+        
     }
 }
-/* if (f2.isFile()) {
-                FileOutputStream fos1 = new FileOutputStream("D:\\a.txt", false);
-                System.out.println("Enter the data want to get write: ");
-                String data = sc.nextLine();
-                byte[] fosArray = data.getBytes();
-                fos1.write(fosArray);
-                System.out.println("Press 1 for copy a.txt in b.txt OR Press 2 for append a.txt in b.txt");
-                int input = sc.nextInt();
-                if (input == 1) {//write mode
-                    FileOutputStream fos2 = new FileOutputStream("D:\\b.txt", false);
-                    fos2.write(fosArray);
-                    Runtime r =Runtime.getRuntime();
-                    String s= "D:\\b.txt";
-                    r.exec("cmd /c start "+s);
-
-                } else if (input == 2) {//append mode
-                    FileOutputStream fos2 = new FileOutputStream("D:\\b.txt", true);
-                    fos2.write(fosArray);
-                    Runtime r =Runtime.getRuntime();
-                    String s= "D:\\b.txt";
-                    r.exec("cmd /c start "+s);*/
